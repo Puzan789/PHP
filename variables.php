2fbc2b7bba2bec00118c1fa94b622d2ae58be1a2
Puzan789 <puzan936@gmail.com>
2023-10-31 20:41:41 +0545

    php started

---
+++ variables.php
@@ -0,0 +1,19 @@
+<?php 
+    $name="Puzan";
+    $food="pizza";
+    $email="fake123@gmail.com";
+    $age=21;
+    $price=4.39;
+
+    $emp=true;
+    $pop=false;
+    echo"Hello {$name} how are you <br>";
+    echo"your favourite food is {$food} <br> ";
+    echo "your email is <strong>{$email}</strong><br>";
+    echo "you are <strong>{$age}</strong> years old <br>";
+    echo"your pizza is \${$price} ";
+    echo "{$emp}";
+    echo "{$pop}";
+
+
+?> 
